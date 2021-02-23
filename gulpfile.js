1729dfe0bf6f17f82345d424e154fc14563b7bdc
Nguyễn Cẩm <cam@Nguyens-MacBook-Pro.local>
2021-02-23 21:22:39 +0700

    Code build

--- gulpfile.js
+++ gulpfile.js
@@ -2,6 +2,7 @@ const gulp = require('gulp');
 const sass = require('gulp-sass');
 const htmlPartial = require('gulp-html-partial');
 const browserSync = require('browser-sync').create();
+const imagemin = require('gulp-imagemin');
 
 function html() {
     return gulp.src('src/*.html')
@@ -14,13 +15,20 @@ gulp.task(html);
 
 function style() {
     return gulp.src('src/scss/**/*.scss')
-    .pipe(sass().on('error',sass.logError))
-    .pipe(gulp.dest('build/asset/css'))
-    .pipe(browserSync.reload({
-        stream: true
-    }))
+        .pipe(sass().on('error',sass.logError))
+        .pipe(gulp.dest('build/asset/css'))
+        .pipe(browserSync.reload({
+            stream: true
+        }))
 }
 gulp.task(style);
+
+function img() {
+    return gulp.src('src/img/**/*')
+            .pipe(imagemin())
+            .pipe(gulp.dest('build/img'))
+}
+gulp.task(img);
 
 function watch() {
     browserSync.init({
@@ -29,9 +37,10 @@ function watch() {
         }
     });
     gulp.watch('src/scss/*.scss', style);
-    gulp.watch('src/*.html').on('change',browserSync.reload);
+    gulp.watch('src/partials/*.html').on('change',gulp.series(html, browserSync.reload));
+    gulp.watch('src/img/*').on('change',gulp.series(img, browserSync.reload));
     gulp.watch('./js/**/*.js').on('change', browserSync.reload);
 }
 gulp.task(watch);
 
-gulp.task('start', gulp.series(html, style, watch));
+gulp.task('start', gulp.series(html, style, img, watch));
